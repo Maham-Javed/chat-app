c611047bb9c9a0ceeb2f3903ad98cc879dfa598d
Maham-Javed <mahamjavedb35@gmail.com>
2024-10-29 16:25:34 +0500

    feature: implement socket.io in app and create basic client-server architechture.

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server); // handle our all socket request.
 
+// Socket.io requests handlers:
+io.on("connection", (socket) => {
+  socket.on("user-message", (message) => {
+    io.emit("message", message);
+  });
+});
+
+// HTTP requests handlers:
 app.use(express.static(path.resolve("./public")));
-const PORT = 9000;
 
 app.get("/", (req, res) => {
   return res.sendFile("/public/index.html");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is rinnumg on PORT: ${PORT}`);
+server.listen(9000, () => {
+  console.log(`Server is rinnumg on PORT: 9000`);
 });
